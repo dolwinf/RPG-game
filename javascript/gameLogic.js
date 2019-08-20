0f0197ba145db66f678ea7a5ca0f7697ed52be96
Dolwin Fernandes <dolwinf@gmail.com>
2019-08-20 16:01:05 +1000

    Added CSS transitions for the first two rows

--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -1,23 +1,42 @@
-$("#goku").on("click", function() {
-  $("#beerus").remove();
-  $("#cell").remove();
-  $("#vegeta").remove();
-});
+$(document).ready(function() {
+  var beerus = $("#beerus");
+  var cell = $("#cell");
+  var vegeta = $("#vegeta");
+  var goku = $("#goku");
 
-$("#beerus").on("click", function() {
-  $("#goku").remove();
-  $("#cell").remove();
-  $("#vegeta").remove();
-});
+  $("#goku").on("click", function() {
+    beerus.remove();
+    cell.remove();
+    vegeta.remove();
+    $("#row2").append(beerus);
+    $("#row2").append(cell);
+    $("#row2").append(vegeta);
+  });
 
-$("#cell").on("click", function() {
-  $("#beerus").remove();
-  $("#goku").remove();
-  $("#vegeta").remove();
-});
+  $("#beerus").on("click", function() {
+    goku.remove();
+    cell.remove();
+    vegeta.remove();
+    $("#row2").append(goku);
+    $("#row2").append(cell);
+    $("#row2").append(vegeta);
+  });
 
-$("#vegeta").on("click", function() {
-  $("#beerus").remove();
-  $("#cell").remove();
-  $("#goku").remove();
+  $("#cell").on("click", function() {
+    goku.remove();
+    beerus.remove();
+    vegeta.remove();
+    $("#row2").append(beerus);
+    $("#row2").append(goku);
+    $("#row2").append(vegeta);
+  });
+
+  $("#vegeta").on("click", function() {
+    goku.remove();
+    cell.remove();
+    beerus.remove();
+    $("#row2").append(beerus);
+    $("#row2").append(cell);
+    $("#row2").append(goku);
+  });
 });
