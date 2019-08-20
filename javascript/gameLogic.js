3641eb267e82e2ce2993d827a2d91b10fa005856
Dolwin Fernandes <dolwinf@gmail.com>
2019-08-20 14:59:28 +1000

    Fixed div card removal alignment upon clicking

--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -1,5 +1,23 @@
 $("#goku").on("click", function() {
-  $("#beerus").empty();
-  $("#cell").empty();
-  $("#vegeta").empty();
+  $("#beerus").remove();
+  $("#cell").remove();
+  $("#vegeta").remove();
+});
+
+$("#beerus").on("click", function() {
+  $("#goku").remove();
+  $("#cell").remove();
+  $("#vegeta").remove();
+});
+
+$("#cell").on("click", function() {
+  $("#beerus").remove();
+  $("#goku").remove();
+  $("#vegeta").remove();
+});
+
+$("#vegeta").on("click", function() {
+  $("#beerus").remove();
+  $("#cell").remove();
+  $("#goku").remove();
 });
