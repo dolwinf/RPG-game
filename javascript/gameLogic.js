c850b1ab6b10f6bd94ed47f20be686baeb6b9294
Dolwin Fernandes <dolwinf@gmail.com>
2019-08-20 16:32:26 +1000

    Added 3rd row JQ code

--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -8,35 +8,119 @@ $(document).ready(function() {
     beerus.remove();
     cell.remove();
     vegeta.remove();
-    $("#row2").append(beerus);
-    $("#row2").append(cell);
-    $("#row2").append(vegeta);
+    $("#row2")
+      .append(beerus)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(beerus);
+        }
+      });
+    $("#row2")
+      .append(cell)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(cell);
+        }
+      });
+    $("#row2")
+      .append(vegeta)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(vegeta);
+        }
+      });
   });
 
   $("#beerus").on("click", function() {
     goku.remove();
     cell.remove();
     vegeta.remove();
-    $("#row2").append(goku);
-    $("#row2").append(cell);
-    $("#row2").append(vegeta);
+    $("#row2")
+      .append(goku)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(goku);
+        }
+      });
+    $("#row2")
+      .append(cell)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(cell);
+        }
+      });
+    $("#row2")
+      .append(vegeta)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(vegeta);
+        }
+      });
   });
 
   $("#cell").on("click", function() {
     goku.remove();
     beerus.remove();
     vegeta.remove();
-    $("#row2").append(beerus);
-    $("#row2").append(goku);
-    $("#row2").append(vegeta);
+    $("#row2")
+      .append(beerus)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(beerus);
+        }
+      });
+    $("#row2")
+      .append(goku)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(goku);
+        }
+      });
+    $("#row2")
+      .append(vegeta)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(vegeta);
+        }
+      });
   });
 
   $("#vegeta").on("click", function() {
     goku.remove();
     cell.remove();
     beerus.remove();
-    $("#row2").append(beerus);
-    $("#row2").append(cell);
-    $("#row2").append(goku);
+    $("#row2")
+      .append(beerus)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(beerus);
+        }
+      });
+    $("#row2")
+      .append(cell)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(cell);
+        }
+      });
+    $("#row2")
+      .append(goku)
+      .on("click", function() {
+        var row3 = $("#row3");
+        if (row3.children().length == 0) {
+          row3.append(goku);
+        }
+      });
   });
 });
