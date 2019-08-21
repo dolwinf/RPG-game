9d1da1eeda9151e2c5301c56673b61fac19ef03f
shell <dolwinf@gmail.com>
2019-08-21 22:35:27 +1000

    Fixed button click bug and added attack and addAttackButton functions

--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -1,212 +1,217 @@
 $(document).ready(function() {
-  var beerus = $("#beerus");
-  var cell = $("#cell");
-  var vegeta = $("#vegeta");
-  var goku = $("#goku");
-  var row1 = $("#row1");
-  var button = $(".btn-primary");
+	var beerus = $("#beerus");
+	var cell = $("#cell");
+	var vegeta = $("#vegeta");
+	var goku = $("#goku");
+	var row1 = $("#row1");
 
-  button.on("click", function() {});
+	var gokuHealth = 100;
+	var vegetaHealth = 100;
+	var beerusHealth = 100;
+	var cellHealth = 100;
 
-  $("#goku").on("click", function() {
-    beerus.remove();
-    cell.remove();
-    vegeta.remove();
-    $("#row2").append(
-      beerus.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(beerus);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Beerus</strong></button>"
-          );
-        }
-      })
-    );
+	function addAttackButton() {
+		if (row1.children().length == 0) {
+			row1.append(attackButton);
+			attack();
+		}
+	}
 
-    $("#row2").append(
-      cell.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(cell);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Cell</strong></button>"
-          );
-        }
-      })
-    );
+	function attack() {
+		var button = $("#attack");
+		button.on("click", function() {
+			console.log("clicked");
+		});
+	}
 
-    $("#row2").append(
-      vegeta.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(vegeta);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Vegeta</strong></button>"
-          );
-        }
-      })
-    );
-  });
+	var attackButton =
+		"<button class='btn btn-primary' id='attack' style='margin-left:1%'>Attack</button>";
 
-  $("#beerus").on("click", function() {
-    goku.remove();
-    cell.remove();
-    vegeta.remove();
-    $("#row2").append(
-      goku.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(goku);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Goku</strong></button>"
-          );
-        }
-      })
-    );
+	$("#goku").on("click", function() {
+		beerus.remove();
+		cell.remove();
+		vegeta.remove();
 
-    $("#row2").append(
-      cell.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(cell);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Cell</strong></button>"
-          );
-        }
-      })
-    );
+		$("#row2").append(
+			beerus.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(beerus);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Beerus</strong></button>"
+					);
+				}
+			})
+		);
 
-    $("#row2").append(
-      vegeta.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(vegeta);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Vegeta</strong></button>"
-          );
-        }
-      })
-    );
-  });
+		$("#row2").append(
+			cell.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(cell);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Cell</strong></button>"
+					);
+				}
+			})
+		);
 
-  $("#cell").on("click", function() {
-    goku.remove();
-    beerus.remove();
-    vegeta.remove();
-    $("#row2").append(
-      beerus.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(beerus);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Beerus</strong></button>"
-          );
-        }
-      })
-    );
+		$("#row2").append(
+			vegeta.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(vegeta);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Vegeta</strong></button>"
+					);
+				}
+			})
+		);
+	});
 
-    $("#row2").append(
-      goku.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(goku);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Goku</strong></button>"
-          );
-        }
-      })
-    );
+	$("#beerus").on("click", function() {
+		goku.remove();
+		cell.remove();
+		vegeta.remove();
+		if (row1.children().length == 0) {
+			row1.append(attackButton);
+		}
+		$("#row2").append(
+			goku.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(goku);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Goku</strong></button>"
+					);
+				}
+			})
+		);
 
-    $("#row2").append(
-      vegeta.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(vegeta);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Vegeta</strong></button>"
-          );
-        }
-      })
-    );
-  });
+		$("#row2").append(
+			cell.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(cell);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Cell</strong></button>"
+					);
+				}
+			})
+		);
 
-  $("#vegeta").on("click", function() {
-    goku.remove();
-    cell.remove();
-    beerus.remove();
-    $("#row2").append(
-      beerus.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(beerus);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Beerus</strong></button>"
-          );
-        }
-      })
-    );
+		$("#row2").append(
+			vegeta.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(vegeta);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Vegeta</strong></button>"
+					);
+				}
+			})
+		);
+	});
 
-    $("#row2").append(
-      cell.on("click", function() {
-        var row3 = $("#row3");
-        console.log(row3.children().length);
-        if (row3.children().length == 0) {
-          console.log("Apending cell");
-          row3.append(cell);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Cell</strong></button>"
-          );
-        }
-      })
-    );
+	$("#cell").on("click", function() {
+		goku.remove();
+		beerus.remove();
+		vegeta.remove();
+		if (row1.children().length == 0) {
+			row1.append(attackButton);
+		}
+		$("#row2").append(
+			beerus.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(beerus);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Beerus</strong></button>"
+					);
+				}
+			})
+		);
 
-    $("#row2").append(
-      goku.on("click", function() {
-        var row3 = $("#row3");
-        if (row3.children().length == 0) {
-          row3.append(goku);
-          row1.append(
-            "<button class='btn btn-primary' style='margin-left:1%'>Attack</button>"
-          );
-          row1.append(
-            "<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Goku</strong></button>"
-          );
-        }
-      })
-    );
-  });
+		$("#row2").append(
+			goku.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(goku);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Goku</strong></button>"
+					);
+				}
+			})
+		);
+
+		$("#row2").append(
+			vegeta.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(vegeta);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Vegeta</strong></button>"
+					);
+				}
+			})
+		);
+	});
+
+	$("#vegeta").on("click", function() {
+		goku.remove();
+		cell.remove();
+		beerus.remove();
+		if (row1.children().length == 0) {
+			row1.append(attackButton);
+		}
+		$("#row2").append(
+			beerus.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(beerus);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Beerus</strong></button>"
+					);
+				}
+			})
+		);
+
+		$("#row2").append(
+			cell.on("click", function() {
+				var row3 = $("#row3");
+				console.log(row3.children().length);
+				if (row3.children().length == 0) {
+					console.log("Apending cell");
+					row3.append(cell);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Cell</strong></button>"
+					);
+				}
+			})
+		);
+
+		$("#row2").append(
+			goku.on("click", function() {
+				var row3 = $("#row3");
+				if (row3.children().length == 0) {
+					row3.append(goku);
+					addAttackButton();
+					row1.append(
+						"<button class='btn btn-warning' style='margin-left: 1%; cursor: default'>You're fighting <strong>Goku</strong></button>"
+					);
+				}
+			})
+		);
+	});
 });
